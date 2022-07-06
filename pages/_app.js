288732a28e5377ca2f38c87c3d75a47f6d0b9ef9
navdesign <navninderbenipal@gmail.com>
2022-07-06 13:51:32 +0530

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function RawJsonTemplates({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='msapplication-TileColor' content='#2d89ef' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default RawJsonTemplates;
